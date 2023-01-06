2bd4a2d5181e02dab4199b281cf6ae23b276c7e7
Vavatrewq <wagnerw651@gmail.com>
2023-01-06 13:30:58 -0300

    PORT PWM

---
+++ src/Porta PWM/script.cpp
@@ -0,0 +1,24 @@
+// C++ code
+//
+void setup()
+{
+  //Valores Serão Escritos para Ascensão Proxima...
+}
+
+void loop()
+{
+	analogWrite(11, 200);
+  	delay(500);
+	analogWrite(11,0);
+	delay(500);
+  
+	analogWrite(10, 30);
+	delay(500);
+	analogWrite(10,0);
+	delay(500);
+  
+	analogWrite(9, 127);
+	delay(500);
+	analogWrite(9,0);
+	delay(500);
+}
